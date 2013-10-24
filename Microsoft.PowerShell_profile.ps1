75c469cb8e1cdc856f5cd07863ecadebf5166b2c
josef.heidegger <j.heidegger@diamonddogs.cc>
2013-10-24 12:01:53 +0200

    added powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,70 @@
+$MaximumHistoryCount = 10KB
+
+if (!(Test-Path ~\PowerShell -PathType Container))
+{   New-Item ~\PowerShell -ItemType Directory
+}
+
+function bye 
+{   Get-History -Count 1KB |Export-CSV ~\PowerShell\history.csv
+    exit
+}
+
+if (Test-path ~\PowerShell\History.csv)
+{   Import-CSV ~\PowerShell\History.csv |Add-History
+}
+
+# set aliases for git
+function GitAddI
+{
+    git add -i
+}
+Set-Alias gitai GitAddI
+function GitStatus
+{
+    git status
+}
+Set-Alias gits GitStatus
+
+function GitCommitM ($Message)
+{
+    git commit -m $Message
+}
+Set-Alias gitcm GitCommitM
+function GitCommitAM ($Message)
+{
+    git commit -am $Message
+}
+Set-Alias gitcam GitCommitAM
+
+function GitPullPush ($Message)
+{
+	git pull
+    git push
+}
+Set-Alias gitpp GitPullPush
+
+function GitPfusch ($Message)
+{
+	git commit -am  "{0}; (fast commit)" -f $MESSAGE 
+	git pull
+    git push
+}
+Set-Alias git-pfusch GitPfusch
+
+# other useful
+function OpenServus ()
+{
+	Start-Process -FilePath "http://servusimbiss.at.st"
+}
+Set-Alias servus OpenServus
+
+function OpenGoldegg ()
+{
+	Start-Process -FilePath "www.facebook.com/cafegoldegg"
+}
+Set-Alias goldegg OpenGoldegg
+Set-Alias ge goldegg
+
+# Load posh-git example profile
+. 'D:\dev\Tools\posh-git\profile.example.ps1'
+
